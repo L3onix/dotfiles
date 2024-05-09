22b134599a635833f4061ad232063512bb46febc
Leonardo Facundes l3onix <leonardo.fm00@gmail.com>
2024-05-09 17:26:17 -0300

    adicionando configurações do lazy-vim

---
+++ lazy-vim/lua/plugins/disabled.lua
@@ -0,0 +1,16 @@
+return {
+  -- flit.nvim
+  { "ggandor/flit.nvim", enabled = false },
+  -- leap.nvim
+  { "ggandor/leap.nvim", enabled = false },
+  -- flash.nvim
+  { "folke/flash.nvim", enabled = false },
+  -- themes
+  { "folke/tokyonight.nvim", enabled = false },
+  { "ellisonleao/gruvbox.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+  -- mini.indentscope
+  -- { "echasnovski/mini.indentscope", enabled = false },
+  -- dashboard-nvim
+  { "nvimdev/dashboard-nvim", enabled = false },
+}
---
+++ lazy-vim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,9 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  -- opts = {
+  --   indent = {
+  --     char = ".",
+  --     tab_char = ".",
+  --   },
+  -- },
+}
