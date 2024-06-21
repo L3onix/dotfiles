4595d173c50b232e888d004ac96b2840e96be70b
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-06-21 14:49:15 -0300

    novo tema de nvim

--- lazy-vim/lua/plugins/colorscheme.lua
+++ lazy-vim/lua/plugins/colorscheme.lua
@@ -1,14 +1,40 @@
 return {
-  "craftzdog/solarized-osaka.nvim",
-  lazy = true,
-  priority = 1000,
-  opts = function()
-    return {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
+  -- Solarized Osaka
+  -- {
+  --   "craftzdog/solarized-osaka.nvim",
+  --   lazy = true,
+  --   priority = 1000,
+  --   opts = {
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "transparent",
+  --     },
+  --   },
+  -- },
+  -- Monokai Nightasty
+  {
+    "polirritmico/monokai-nightasty.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      dark_style_background = "transparent",
+      light_style_background = "transparent",
+      color_headers = true,
+      lualine_bold = true,
+      lualine_style = "default",
+      hl_styles = {
+        comments = { italic = true },
+        keywords = { italic = true },
         floats = "transparent",
+        sidebars = "transparent",
       },
-    }
-  end,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "monokai-nightasty",
+    },
+  },
 }
