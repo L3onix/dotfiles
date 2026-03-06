bc7d376c25f95fb65ca623d83fd3c1b4ccb38115
Leonardo Facundes l3onix <leonardo.fm00@gmail.com>
2026-03-06 15:48:28 -0300

    adicionando atalho para fechar buffer

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Fechar aba => Ctrl + w
+keymap.set("n", "<C-w>", "<cmd>bdelete<cr>")
