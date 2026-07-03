32d35fe5854c0693fef5b902a384b6958f283479
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2026-07-03 00:58:54 -0300

    adicionando script para criar links simbolicos dos diretorios de configuração

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## alacritty
+rm -rf ~/.config/alacritty && ln -s $PWD/alacritty ~/.config/alacritty
+## hyprland
+rm -rf ~/.config/hypr && ln -s $PWD/hypr ~/.config/hypr
+## kitty
+rm -rf ~/.config/kitty && ln -s $PWD/kitty ~/.config/kitty
+## mako
+rm -rf ~/.config/mako && ln -s $PWD/mako ~/.config/mako
+## MangoHud
+rm -rf ~/.config/MangoHud && ln -s $PWD/MangoHud ~/.config/MangoHud
+## rofi
+rm -rf ~/.config/rofi && ln -s $PWD/rofi ~/.config/rofi
+## scripts
+rm -rf ~/.config/scripts && ln -s $PWD/scripts ~/.config/scripts
+## swaync
+rm -rf ~/.config/swaync && ln -s $PWD/swaync ~/.config/swaync
+## tmux
+rm -rf ~/.config/tmux && ln -s $PWD/tmux ~/.config/tmux
+## waybar
+rm -rf ~/.config/waybar && ln -s $PWD/waybar ~/.config/waybar
+## wofi
+rm -rf ~/.config/wofi && ln -s $PWD/wofi ~/.config/wofi
