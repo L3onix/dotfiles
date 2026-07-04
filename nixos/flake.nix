27cee4fbf776c3bfa950d1fe445dbd19a5a8c303
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2026-07-04 00:12:02 -0300

    corrigindo hostname referenciado no flake

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,7 +2,7 @@
 	inputs.hyprland.url = "github:hyprwm/Hyprland";
 
 	outputs = {nixpkgs, ...} @ inputs: {
-		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+		nixosConfigurations.green-bulba = nixpkgs.lib.nixosSystem {
 			specialArgs = { inherit inputs; };
 			modules = [
 				./configuration.nix
